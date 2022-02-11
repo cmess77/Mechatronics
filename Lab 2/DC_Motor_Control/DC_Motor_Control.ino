d416f5eecf9fe534a5aca071b685f7b19c23971d
Cameron Messer <cmess77@protonmail.com>
2022-02-10 22:00:32 -0500

    Add comments

--- Lab 2/DC_Motor_Control/DC_Motor_Control.ino
+++ Lab 2/DC_Motor_Control/DC_Motor_Control.ino
@@ -1,33 +1,63 @@
+//defining pin numbers in global space
 #define motorEnable 10
 #define input1 12
 #define input2 13
 #define pot A0
 
+//declaring variables for potentiometer position, motor speed input signal
 int potPos, dcInput;
+float inputVoltage;
 
 void setup() {
+  //starting serial communication
   Serial.begin(9600);
+  //printing header for output table
+  Serial.println("V(in), Output");
+
+  //setting output pins for enable, in1 and in2 pins on L293D
   pinMode(motorEnable, OUTPUT);
   pinMode(input1, OUTPUT);
   pinMode(input2, OUTPUT);
+
+  //setting input pin for potentiometer
   pinMode(pot, INPUT);
 }
 
 void loop() {
- potPos = analogRead(pot);
+  //reading pot position using analog input signal
+  potPos = analogRead(pot);
 
- if(potPos <= 512) {
-  dcInput = map(potPos, 0, 512, 255, 0);
-  analogWrite(motorEnable, dcInput);
-  digitalWrite(input1, LOW);
-  digitalWrite(input2, HIGH);
- }
+  //case 1: pot in lower half, motor running counterclockwise
+  if(potPos <= 512) {
+    //mapping 10bit analog input value to analog out value, PWM decreases with analog signal increase
+    dcInput = map(potPos, 0, 512, 255, 0);
 
- else if(potPos > 512) {
-  dcInput = map(potPos, 513, 1023, 0, 255);
-  analogWrite(motorEnable, dcInput);
-  digitalWrite(input1, HIGH);
-  digitalWrite(input2, LOW);
- }
+    //setting enable pin to PWM signal from dcInput, and writing in1 HIGH and in2 LOW (counterclockwise)
+    analogWrite(motorEnable, dcInput);
+    digitalWrite(input1, LOW);
+    digitalWrite(input2, HIGH);
+
+    //printing needed table values
+    inputVoltage = potPos * (5.0 / 1023.0);
+    Serial.print(inputVoltage);
+    Serial.println(dcInput);
+  }
+
+  //case 2: pot in upper half, motor running clockwise
+  else if(potPos > 512) {
+    //mapping 10bit analog value to analog out value, PWM increases with analog signal increase
+    dcInput = map(potPos, 513, 1023, 0, 255);
+
+    //setting enable pin to PWM signal from dcInput, and writing in1 LOW and in2 HIGH 
+    analogWrite(motorEnable, dcInput);
+    digitalWrite(input1, HIGH);
+    digitalWrite(input2, LOW);
+
+    //printing needed table values
+    float inputVoltage = potPos * (5.0 / 1023.0);
+    Serial.print(inputVoltage);
+    Serial.print(", ");
+    Serial.println(dcInput);
+  }
 
 }
--- Lab 2/stepper_motor_control/stepper_motor_control.ino
+++ Lab 2/stepper_motor_control/stepper_motor_control.ino
@@ -1,16 +1,24 @@
+//defining pin numbers in global space
 #define pot A0
 #define in1 7
 #define in2 6
 #define in3 5
 #define in4 4
 
+//declaring variables for potentiometer position, stepper motor step-delay
 int potPos, delayTime;
+float inputVoltage;
 
 void setup(void) {
+  //starting serial communication
   Serial.begin(9600);
+  //printing header for output table
   Serial.println("PotPos, delayTime");
+
+  //setting input pin for potentiometer
   pinMode(pot, INPUT);
-  
+
+  //setting output pins for coil1, coil2, coil3, and coil4 on ULN2003
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
   pinMode(in3, OUTPUT);
@@ -19,26 +27,42 @@ void setup(void) {
 
 
 void loop(void) {
- while((potPos = analogRead(pot)) <= 512) {
-  delayTime = map(potPos, 0, 512, 800,10000);
-  reverseDrive(delayTime);
+  //case 1: pot in lower half, motor running counterclockwise
+  while((potPos = analogRead(pot)) <= 512) { //this line reads/updates potPos every loop iteration
+    //mapping 10bit analog input value to analog output value, delayTime increases with analog signal increase
+    delayTime = map(potPos, 0, 512, 800,10000);
+    //calls reverse drive function with delayTime as its only parameter
+    reverseDrive(delayTime);
 
-  Serial.print(potPos);
-  Serial.print(", ");
-  Serial.println(delayTime);
- }
+    //calculating input voltage from analog signal
+    inputVoltage = potPos * (5.0/1023.0);
 
- while((potPos = analogRead(pot)) > 512) {
-  delayTime = map(potPos, 513, 1023, 10000,800);
-  forwardDrive(delayTime);
-
-  Serial.print(potPos);
-  Serial.print(", ");
-  Serial.println(delayTime);
+    //printing needed table values
+    Serial.print(inputVoltage);
+    Serial.print(", ");
+    Serial.println(delayTime);
   }
+
+  //case 2: pot in upper half, motor running clockwise
+  while((potPos = analogRead(pot)) > 512) {
+    //mapping 10bit analog input value to analog output value, delayTime decreases 
+    //with analog signal increase
+    delayTime = map(potPos, 513, 1023, 10000,800);
+    //calls forward drive function with delayTime as its only parameter
+    forwardDrive(delayTime);
+
+    //calculating input voltage from analog signal
+    inputVoltage = potPos * (5.0/1023.0);
+
+    //printing needed table values
+    Serial.print(inputVoltage);
+    Serial.print(", ");
+    Serial.println(delayTime);
+    }
 }
 
 
+//function in order to organize digitalWrite actions, makes code cleaner
 void pinWrite(int pin1,int pin2,int pin3,int pin4) {
   digitalWrite(in1,pin1);
   digitalWrite(in2,pin2);
@@ -47,7 +71,10 @@ void pinWrite(int pin1,int pin2,int pin3,int pin4) {
 }
 
 
+//function in order to complete 1 step of the stepper motor (counterclockwise)
 void reverseDrive(int us){
+  //calls pinWrite 8 times to make 1 step of the motor, with "us" microsecond delay between each
+  //coil activation
   pinWrite(1,0,0,0);
   delayMicroseconds(us);
   pinWrite(1,1,0,0);
@@ -66,8 +93,10 @@ void reverseDrive(int us){
   delayMicroseconds(us);
 }
 
-
+//function in order to complete 1 step of the stepper motor (clockwise)
 void forwardDrive(int us){
+  //calls pinWrite 8 times to make 1 step of the motor, with "us" microsecond delay between each
+  //coil activation
   pinWrite(0,0,0,1);
   delayMicroseconds(us);
   pinWrite(0,0,0,1);
