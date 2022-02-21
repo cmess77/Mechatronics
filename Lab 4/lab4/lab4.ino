ce31930b419ccae681baf352ec678e739ca1429a
Cameron Messer <cmess77@protonmail.com>
2022-02-21 15:30:05 -0500

    Modify lab4.ino

--- Lab 4/lab4/lab4.ino
+++ Lab 4/lab4/lab4.ino
@@ -5,10 +5,27 @@
 #define photoPin A1
 #define thermPin A0
 
+int photoData, thermData,timeElapse;
+float duration,distance, ultraData;
+
 void setup() {
     Serial.begin(9600);
+
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
 }
 
 void loop() {
-    
+    //photoData = analogRead(photoPin);
+    //Serial.println(photoData);
+    //delay(100);
+
+    digitalWrite(trigPin,LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    duration = pulseIn(echoPin, HIGH);
+    ultraData = (duration * 0.0345) * 0.5; //in cenimeters
+    Serial.println(ultraData);
 }
