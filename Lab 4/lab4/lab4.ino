991effb50ce50009c5a963157824cf11ba653c97
Cameron Messer <cmess77@protonmail.com>
2022-03-08 19:30:36 -0500

    Remove Lab4.xlsx, modify lab4.ino

--- Lab 4/lab4/lab4.ino
+++ Lab 4/lab4/lab4.ino
@@ -5,8 +5,8 @@
 #define photoPin A1
 #define thermPin A0
 
-int photoData, thermData,timeElapse;
-float duration,distance, ultraData;
+int photoData, thermData, timeElapse;
+float duration, ultraPositionInches, ultraPositionFeet;
 
 void setup() {
     Serial.begin(9600);
@@ -19,8 +19,8 @@ void setup() {
 
 void loop() {
     //photoSensor();
-    thermSensor();
-    //ultraSensor();
+    //thermSensor();
+    ultraSensor();
 }
 
 void ultraSensor(void) {
@@ -29,9 +29,14 @@ void ultraSensor(void) {
     digitalWrite(trigPin, HIGH);
     delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
+
     duration = pulseIn(echoPin, HIGH);
-    ultraData = (duration * 0.0345) * 0.5; //in cenimeters
-    Serial.println(ultraData);
+    //ultraPositionInches = (duration * 0.0345) * 0.5 * 0.3937008; //in inches
+    ultraPositionFeet = (duration * 0.0345) * 0.5 * 0.3937008 / 12; //in feet
+
+    //Serial.println(ultraPositionInches);
+    Serial.println(ultraPositionFeet);
+    delay(100);
 }
 
 void photoSensor(void) {
